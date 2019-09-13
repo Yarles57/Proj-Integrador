6837370a78f8c6a01d1026733cec050edd5bbbad
João Denílson Souza dos Santos <denilsonsantos021@gmail.com>
2019-09-13 00:08:40 -0300

    Update cadasLab.php
    Foi adicionado style na p[agina. Código CSS. E modificado no link do form de autenticar para index.

--- View/cadasLab.php
+++ View/cadasLab.php
@@ -4,20 +4,44 @@
 	<title>Cadastrar Laboratório</title>
 	<meta charset="utf-8">
 	<!--<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">-->
+	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+	 <style type="text/css">
+    *{
+      margin: 0;
+      padding: 0;
+      font-family: Calibri;
+      font-variant: small-caps;
+    }
+    #container{
+      margin-top: 50%;
+      width: 25%;
+      margin: 0 auto;
+      border: 1px solid#3399ff;
+      min-width: 10%;
 
+    }
+    h1{
+      color: #FFF;
+      font-size: 1.8em;
+      font-weight: lighter;
+    }
+  </style>
 </head>
 <body>
-	<form action="../autenticar.php" method="post">
+<div id="container" style= "margin-top: 10%";>
+		<h1 class="navbar navbar-dark bg-dark">Cadastrar Laboratório</h1>
+		<form action="../index.php" method="post">
 
-		<label>Nome: <input type="text" name="nomeLab" required="on"></label><br />
-		<label>Código: <input type="text" name="codeLab" required="on"></label><br />
-		<label>Qtd. Computadores: <input type="number" name="qtdComputadores" required="on"></label><br />
+			<label>Nome: <input type="text" class="form-control" name="nomeLab" required="on"></label><br />
+			<label>Código: <input type="text" class="form-control" name="codeLab" required="on"></label><br />
+			<label>Qtd. Computadores: <input type="number" min="1" class="form-control" name="qtdComputadores" required="on"></label><br />
 
-		<input type="hidden" name="metodo" value="store">
-        <input type="hidden"  name="classe" value="laboratorio">
+			<input type="hidden" name="metodo" value="store">
+	        <input type="hidden"  name="classe" value="laboratorio">
 
-		<input type="submit" value="Salvar">
+			<input type="submit" value="Salvar" class="btn btn-primary">
 
-	</form>
+		</form>
+	</div>
 </body>
 </html>
