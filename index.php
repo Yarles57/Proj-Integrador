a50eae0c615e35e515b7b811b199a2a84713fadb
João Denílson Souza dos Santos <denilsonsantos021@gmail.com>
2019-09-13 00:20:43 -0300

    Update index.php
    Foi altereado onde tinha "LaboratórioId" por "id".

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
 	if(isset($_GET['classe']) && isset($_GET['metodo'])){
 		$classe = $_GET['classe'].'Controller';
 		$metodo = $_GET['metodo'];
-		if (isset($_GET['laboratorioId'])) {
-			$fornecedorId = $_GET['laboratorioId'];
+		if (isset($_GET['id'])) {
+			$id = $_GET['id'];
 		}
 		
 	}
 	else if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
 		$classe= $_POST['classe'].'Controller';
 		$metodo= $_POST['metodo'];
-		if (isset($_POST['laboratorioId'])){
-			$laboratorioId = $_POST['laboratorioId'];
+		if (isset($_POST['id'])){
+			$id = $_POST['id'];
 		}
 	}
 	else{
@@ -23,11 +23,11 @@
 
 	$controller = new $classe();
 
-	if(!$laboratorioId){
+	if(!$id){
 		$controller->$metodo();
 	}	
 	else  {
-		$controller->$metodo($laboratorioId);
+		$controller->$metodo($id);
 	}
 	
 
