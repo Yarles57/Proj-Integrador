366ad0738140b331d4a338c578148cfe1c7e2d28
João Denílson Souza dos Santos <denilsonsantos021@gmail.com>
2019-09-13 00:09:12 -0300

    Update editarLab.php
    Foi adicionado style na p[agina. Código CSS.

--- View/editarLab.php
+++ View/editarLab.php
@@ -20,37 +20,53 @@
     <meta charset="UTF-8"/>
     <title>Editar Laboratório</title>
     <meta charset="utf-8">
+
+    <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+
+    <style type="text/css">
+    *{
+      margin: 0;
+      padding: 0;
+      font-family: Calibri;
+      font-variant: small-caps;
+    }
+    #container{
+      margin-top: 50%;
+      width: 25%;
+      margin: 0 auto;
+      border: 1px solid#3399ff;
+      min-width: 10%;
+
+    }
+    h1{
+      color: #FFF;
+      font-size: 1.8em;
+      font-weight: lighter;
+    }
+  </style>
+
   </head>
 
   <body>
 
-      <h3>Alterar dados de Fornecedor</h3>
+      <div id="container" style= "margin-top: 10%";>
+      <h1 class="navbar navbar-dark bg-dark">Alterar dados do Laboratório</h1>
+      <form method="post" action="../index.php">
 
-      <form method="post" action="../autenticar.php">
-
-          
-
-          <label>Nome: </label>
-          <input type="text"  name="nome" value= "<?php echo "$nomeLaboratorio";?>" /><br />
+          <label>Nome:
+          <input type="text"  class="form-control" name="nome" value= "<?php echo "$nomeLaboratorio";?>" /></label><br />
        
-
+          <label>Código: 
+          <input type="text"  class="form-control" name="endereco" value="<?php echo "$codigoLaboratorio";?>"></label><br />
        
-          <label>Código: </label>
-          <input type="text"  name="endereco" value="<?php echo "$codigoLaboratorio";?>"><br />
-       
-
-        
-          <label>Qtd. Computadores: </label>
-          <input type="text" name="telefone" value="<?php echo "$qtdcompLab";?>"><br />
+          <label>Qtd. Computadores: 
+          <input type="text" class="form-control" name="telefone" value="<?php echo "$qtd";?>"></label><br />
               
-
-        <!-- passa para o index via POST os valores da classe e metodo, juntamente com o ID, para que possa ser atualizado no banco os novos dados-->
-        
         <input type="hidden" name="labId" value="<?php echo "$id";?>">
         <input type="hidden" name="metodo" value="update">
         <input type="hidden"  name="classe" value="laboratorio">
 
-        <button type="submit">Alterar</button>
+        <button type="submit" class="btn btn-primary">Alterar</button>
 
       </form>
     </div>
