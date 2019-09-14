6a912bf968eed4d2004e4fd745f2cd4f5375f2db
YarlesTorres <yarles57@gmail.com>
2019-09-14 15:53:20 -0300

    Update listarLab.php

--- View/listarLab.php
+++ View/listarLab.php
@@ -1,26 +1,24 @@
 <?php
   require_once "../Model/labModel.php";
+
   session_start();
-
   $lab = $_SESSION['laboratorios'];
-
+  //$lab = array();
 ?>
 
 <!DOCTYPE html>
-<html>
-
+<html lang="PT-BR">
   <head>
-    <meta charset="UTF-8"/>
-    <title></title>
+    <title>Lista de Laboratórios</title>
+    <meta charset="UTF-8"/>    
 
     <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
   </head>
 
   <body>
-
     <div class="container">
       <h3>Lista de Laboratórios</h3>
-      
+     
       <table border="1" class="table">
         <thead class="thead-dark">
             <tr>
@@ -30,6 +28,7 @@
               <th colspan="2">Ação</th>    
             </tr>
         </thead>
+
        <?php
           foreach($lab as $aux){
             echo "<tr>";
@@ -45,7 +44,7 @@
             echo '<td><a href="../index.php?classe=laboratorio&metodo=delete&id='.$aux->getIdLab().'">Deletar</a></td>';
             echo "</tr>";
             
-          }
+          };
         ?>
 
       </table>
