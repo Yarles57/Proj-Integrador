c19d5fc254ed5639415fc10957e6624bb94b26b3
FcoGabrielSL <fcogabriel373@gmail.com>
2019-09-15 18:08:59 -0300

    Update listarLab.php

--- View/listarLab.php
+++ View/listarLab.php
@@ -34,6 +34,29 @@
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
       <i class="fas fa-bars"></i>
     </button>
+     <!-- Navbar Search -->
+    <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+      <div class="input-group">
+      
+        </div>
+      </div>
+    </form>
+
+    <ul class="navbar-nav ml-auto ml-md-0">
+
+
+      <li class="nav-item dropdown no-arrow">
+        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user-circle fa-fw"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+          <a class="dropdown-item" href="#">Configurações</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Sair</a>
+        </div>
+      </li>
+    </ul>
+
 
 
   </nav>
@@ -63,7 +86,7 @@
         
         <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i class="fas fa-fw fa-folder"></i>
-          <span>Ger. Profrofessor</span>
+          <span>Ger. Professor</span>
         </a>
         
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
