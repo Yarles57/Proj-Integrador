f3d08a5c173eecdcde4a89498859def4798d1f87
FcoGabrielSL <fcogabriel373@gmail.com>
2019-09-15 17:31:24 -0300

    Update cadasLab.php

--- View/cadasLab.php
+++ View/cadasLab.php
@@ -116,14 +116,14 @@
             <div class="form-row">
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input type="text" id="firstName" class="form-control" placeholder="First name" required="on" autofocus="autofocus" name="nomeLab">
+                  <input  type="text" id="firstName" class="form-control" placeholder="First name" required="on" autofocus="autofocus" name="nomeLab">
                   <label for="firstName">Nome do Laboratório</label>
                 </div>
                 </div>
               
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input type="text" id="lastName" class="form-control" placeholder="Código do Laboratório" name="codeLab" required="on">
+                  <input type="text" id="lastName" class="form-control" maxlength="6" placeholder="Código do Laboratório" name="codeLab" required="on">
                   <label for="lastName">Código do Laboratório</label>
                 </div>
               </div>
@@ -131,63 +131,21 @@
           </div>
           <div class="form-group">
             <div class="form-label-group">
-              <input input type="number" min="1" id="inputEmail" class="form-control" placeholder="Quantidade de Computadores" name="qtdComputadores" required="on">
+              <input input type="number" min="1" max="30" id="inputEmail" class="form-control" placeholder="Quantidade de Computadores" name="qtdComputadores" required="on">
               <label for="inputEmail">Quantidade de Computadores</label>
             </div>
           </div>
 
 			   <input type="hidden" name="metodo" value="store">
 	        <input type="hidden"  name="classe" value="laboratorio">
-	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
-	 <style type="text/css">
-    *{
-      margin: 0;
-      padding: 0;
-      font-family: Calibri;
-      font-variant: small-caps;
-    }
-    #container{
-      margin-top: 50%;
-      width: 25%;
-      margin: 0 auto;
-      border: 1px solid#3399ff;
-      min-width: 10%;
 
-    }
-    h1{
-      color: #FFF;
-      font-size: 1.8em;
-      font-weight: lighter;
-    }
+<input type="submit" value="Salvar" class="btn btn-primary">
 
-  </style>
+    </form>
+  </div>
 
-</head>
-<body>
-	<div id="container" style= "margin-top: 10%";>
-		<h1 class="navbar navbar-dark bg-dark">Cadastrar Laboratório</h1>
 
-		<form action="../index.php" method="post">
-
-			<label>Nome:
-        <input type="text" class="form-control" name="nomeLab" placeholder="Ex: Laboratório de Redes" required="on" autofocus>
-      </label><br />
-			
-      <label>Código: 
-        <input type="text" class="form-control" name="codeLab" maxlength="6" placeholder="Ex: Lab001" required="on" >
-      </label><br />
-			
-      <label>Qtd. Computadores: 
-        <input type="number" min="1" max="30" class="form-control" name="qtdComputadores" placeholder="Ex: 20" required="on" >
-      </label><br />
-
-			<input type="hidden" name="metodo" value="store">
-	    <input type="hidden"  name="classe" value="laboratorio">
-
-			<input type="submit" value="Salvar" class="btn btn-primary">
-
-		</form>
-	</div>
+		
 
   <!-- Bootstrap core JavaScript-->
   <script src="../vendor/jquery/jquery.min.js"></script>
