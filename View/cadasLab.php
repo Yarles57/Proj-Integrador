01c82f8fe8f30c624e3180e2c6395315cec383f9
FcoGabrielSL <fcogabriel373@gmail.com>
2019-09-16 16:44:06 -0300

    Update cadasLab.php

--- View/cadasLab.php
+++ View/cadasLab.php
@@ -130,6 +130,7 @@
         <form action="../index.php" method="post">
           <div class="form-group">
             <div class="form-row">
+              
               <div class="col-md-6">
                 <div class="form-label-group">
                   <input  type="text" id="firstName" class="form-control" placeholder="First name" required="on" autofocus="autofocus" name="nomeLab">
@@ -151,11 +152,15 @@
               <label for="inputEmail">Quantidade de Computadores</label>
             </div>
           </div>
+        
+      </div>
+      
+
 
 			   <input type="hidden" name="metodo" value="store">
 	        <input type="hidden"  name="classe" value="laboratorio">
 
-<input type="submit" value="Salvar" class="btn btn-primary">
+      <input type="submit" value="Salvar" class="btn btn-primary">
 
     </form>
   </div>
