9e3446eac40c5b99f09fe3657fb15bbaec1df121
João Denílson Souza dos Santos <denilsonsantos021@gmail.com>
2019-09-16 18:55:14 -0300

    Update professorController.php

--- Controller/professorController.php
+++ Controller/professorController.php
@@ -22,7 +22,8 @@
 
 		public function store(){
 			$cursoProf = $_POST['cursoProf'];
-	        $loginProf = $_POST['loginProf'];
+			$loginProf = $_POST['loginProf'];
+			//md5 é para criptografar a senha
 	        $senhaProf = md5($_POST['senhaProf']);
 	        $celProf = $_POST['celProf'];
 	        $emailProf = $_POST['emailProf'];
