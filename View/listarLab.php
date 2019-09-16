67d1b651400a67b0f0d079559dbe0c98431ee6bd
João Denílson Souza dos Santos <denilsonsantos021@gmail.com>
2019-09-16 16:45:51 -0300

    Update listarLab.php

--- View/listarLab.php
+++ View/listarLab.php
@@ -2,7 +2,7 @@
   require_once "../Model/labModel.php";
   session_start();
   $lab = $_SESSION['laboratorios'];  
-  $lab = $_SESSION['laboratorios'];
+  //$lab = $_SESSION['laboratorios'];
 ?>
 
 <!DOCTYPE html>
