343cdad1fcb04874ff162a43e1c53b981f38c21e
YarlesTorres <yarles57@gmail.com>
2019-09-17 21:56:35 -0300

    Update cadasProf.php

--- View/cadasProf.php
+++ View/cadasProf.php
@@ -146,22 +146,22 @@
 
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input  type="text" id="loginProf" class="form-control" placeholder="Login do Professor" required="on" autofocus="autofocus" name="loginProf">
-                  <label for="loginProf">Login do Professor</label>
+                  <input  type="text" id="loginProf" class="form-control" placeholder="Nome do Professor" required="on" autofocus="autofocus" name="loginProf">
+                  <label for="loginProf">Nome do Professor</label>
                 </div>
               </div>
 
 
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input  type="text" id="senhaProf" class="form-control" placeholder="Senha do Professor" required="on" autofocus="autofocus" name="senhaProf">
+                  <input  type="password" id="senhaProf" class="form-control" placeholder="Senha do Professor" required="on" autofocus="autofocus" name="senhaProf">
                   <label for="senhaProf">Senha do Professor</label>
                 </div>
               </div>
 
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input  type="text" id="celProf" class="form-control" placeholder="Senha do Professor" required="on" autofocus="autofocus" name="celProf">
+                  <input  type="text" id="celProf" class="form-control" placeholder="N° para contato do Professor" required="on" autofocus="autofocus" name="celProf">
                   <label for="celProf">N° para contato do Professor</label>
                 </div>
               </div>
