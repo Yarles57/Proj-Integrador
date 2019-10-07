6b2a12f391f02e49a8fde5f9620c735e6cde621a
FcoGabrielSL <fcogabriel373@gmail.com>
2019-10-07 20:45:31 -0300

    Update editarLab.php

--- View/editarLab.php
+++ View/editarLab.php
@@ -15,128 +15,15 @@
 <!DOCTYPE html>
 <html lang="PT-BR">
 <head>
-  <title>Editar Laboratório</title>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-   <!-- Custom fonts for this template-->
-  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
-  <!-- Custom styles for this template-->
-  <link href="../css/sb-admin.css" rel="stylesheet">
-  <link href="../css/sb-img.css" rel="stylesheet">
+  <title>SisRes - Editar Laboratório</title>
 
 
 </head>
 <body class="bg-dark">
-  <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
-    <a class="navbar-brand mr-1" href="dashboard.php"></a>
-
-    <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="../dashboard.php">
-      <i class="fas fa-bars"></i>
-    </button>
-
-    <!-- Navbar Search -->
-    <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-      <div class="input-group">
-      
-        </div>
-      </div>
-    </form>
-
-    <ul class="navbar-nav ml-auto ml-md-0">
-
-
-      <li class="nav-item dropdown no-arrow">
-        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-user-circle fa-fw"></i>
-        </a>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-          <a class="dropdown-item" href="#">Configurações</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Sair</a>
-        </div>
-      </li>
-    </ul>
-
-    
-
-    <!-- Navbar -->
-
-  </nav>
-  <div id="wrapper">
-
-    <!-- Sidebar -->
-    <ul class="sidebar navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="dashboard.php">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Painel de Controle</span>
-        </a>
-      </li>
-      <li class="nav-item dropdown">
-        
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-fw fa-folder"></i>
-          <span>Ger. Laboratório</span>
-        </a>
-        
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-         
-          <h6 class="dropdown-header">Gerenciar Laboratório:</h6>
-          <a class="dropdown-item" href="cadasLab.php?classe=laboratorio&metodo=create">Cadastrar</a>
-          <a class="dropdown-item" href="listarLab.php?classe=laboratorio&metodo=index">Listar</a>
-          <div class="dropdown-divider"></div>
-        </div>
-
-      </li>
-
-      <li class="nav-item dropdown">
-        
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-fw fa-folder"></i>
-          <span>Ger. Profrofessor</span>
-        </a>
-        
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-
-          <h6 class="dropdown-header">Gerenciar Professor:</h6>
-          <a class="dropdown-item" href="cadasProf.php?classe=professor&metodo=create">Cadastrar</a>
-          <a class="dropdown-item" href="listarProf.php?classe=professor&metodo=index">Listar</a>
-          <div class="dropdown-divider"></div>
-        </div>
-
-      </li>
-
-      <li class="nav-item dropdown">
-        
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-fw fa-folder"></i>
-          <span>Ger. Reserva</span>
-        </a>
-        
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-          <h6 class="dropdown-header">Gerenciar Reservas: </h6>
-          <a class="dropdown-item" href="listarRes.php?classe=reserva&metodo=index">Listar</a>
-        </div>
-
-      </li>
-      
-    </ul>
-
-    <div id="content-wrapper">
-
-      <div class="container-fluid">
-
-        <!-- Breadcrumbs-->
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">
-            <a href="dashboard.php">Painel de Controle</a>
-          </li>
-          <li class="breadcrumb-item active">Editar Laboratório</li>
+  <?php
+    include "menu/menu.php";
+  ?>
+          <li class="breadcrumb-item active"> / Editar Laboratório</li>
         </ol>
 
     <div class="container">
