18bd66dfc0086f0ca4033c7b890ca718e8aba93b
FcoGabrielSL <fcogabriel373@gmail.com>
2019-10-07 21:09:41 -0300

    Update listarProf.php

--- View/listarProf.php
+++ View/listarProf.php
@@ -77,40 +77,6 @@
 
      <a href="../indexProf.php?classe=professor&metodo=create">Adicionar</a>
 
-       
-
-     <!-- Bootstrap core JavaScript-->
-  <script src="../vendor/jquery/jquery.min.js"></script>
-  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Page level plugin JavaScript-->
-  <script src="../vendor/chart.js/Chart.min.js"></script>
-  <script src="../vendor/datatables/jquery.dataTables.js"></script>
-  <script src="../vendor/datatables/dataTables.bootstrap4.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="../js/sb-admin.min.js"></script>
-
-  <!-- Demo scripts for this page-->
-  <script src="../js/demo/datatables-demo.js"></script>
-  <script src="../js/demo/chart-area-demo.js"></script>
-      
-   <script>
-    $(function(){
-      $(".delete").on('click', function(e) {
-        e.preventDefault();
-        var delid = $(this).attr("delid");
-
-        if (confirm('Deseja deletar esse Professor')) {
-            window.location.replace("../indexProf.php?classe=professor&metodo=delete&id="+delid);
-        }
-      });
-    });
-  </script> 
-
   </body>
 
 </html>
