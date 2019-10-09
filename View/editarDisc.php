8b30714b975f46e3ee49ecdd4561f6cd9dbdfa09
YarlesTorres <yarles57@gmail.com>
2019-10-09 16:32:38 -0300

    Update editarDisc.php

--- View/editarDisc.php
+++ View/editarDisc.php
@@ -1,13 +1,22 @@
 <?php
-  require_once "../Model/disciplinaModel.class.php";
+  require_once "../Model/cursoModel.php";
+
   session_start();
+  if(!empty( $_SESSION['cursos'])){
+     $cursos = $_SESSION['cursos'];
+  }
+  else{
+    //header("Location: ../indexCurso.php");
+  }
+
+  require_once "../Model/disciplinaModel.php";
   $disc = $_SESSION['editaDisc'];
 
   foreach($disc as $aux){
     $idDisc = $aux['idDisciplina'];
     $nomeDisc = $aux['nomeDisciplina'];
     $siglaDisc = $aux['siglaDisciplina'];
-    $cursoDisc = $aux['cursoDisciplina'];
+    $cursoDisc = $aux['idCursoFk'];
   } 
 
 ?>
@@ -36,29 +45,33 @@
             <div class="form-row">
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input type="text" id="firstName" class="form-control" placeholder="First name" required="on" autofocus="autofocus" name="nomeDisciplina" value= "<?php echo "$nomeDisciplina";?>">
+                  <input type="text" id="firstName" class="form-control" placeholder="First name" required="on" autofocus="autofocus" name="nomeDisciplina" value= "<?php echo "$nomeDisc";?>">
                   <label for="firstName">Nome da Disciplina</label>
                 </div>
               </div>
               
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input type="text" id="lastName" class="form-control" placeholder="Sigla da Disciplina" name="codigoLab" required="on" value="<?php echo "$siglaDisciplina";?>" >
+                  <input type="text" id="lastName" class="form-control" placeholder="Sigla da Disciplina" name="siglaDisc" required="on" value="<?php echo "$siglaDisc";?>" >
                   <label for="lastName">Sigla da Disciplina</label>
                 </div>
               </div>
             </div>
           </div>
 
-          <select class="btn btn-info dropdown-toggle" value="<?php echo "$cursoDisciplina";?>">>
-            <option> Curso da Disciplina</option>
-            <option value="bsis" >BSI</option>
-            <option value="lfis" >Física</option>
-            <option value="tmec" >Mecatronica</option>
-            <option value="lmat" >Matemática</option>
-          </select>
+         
 
-        <input type="hidden" name="id" value="<?php echo "$idDisciplina";?>">
+         <select class="btn btn-info dropdown-toggle" name ="cursoDisc">
+          <option selected="selected" name = "cursoDisc"> Curso da Disciplina</option>
+          <?php
+            foreach ($cursos as $curso) {
+               echo "<option value='{$curso->getIdCurso()}' >{$curso->getNomeCurso()}</option>";
+            }
+           
+          ?>
+        </select>
+
+        <input type="hidden" name="id" value="<?php echo "$idDisc";?>">
         <input type="hidden" name="metodo" value="update">
         <input type="hidden"  name="classe" value="disciplina">
 
