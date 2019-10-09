6b0275c8d8b1c4ff775e724de3110429e83ac8bd
YarlesTorres <yarles57@gmail.com>
2019-10-08 21:29:22 -0300

    Create indexUser.php

---
+++ indexUser.php
@@ -0,0 +1,33 @@
+<?php
+	include "Controller/usuarioController.php";
+	
+	if(isset($_GET['classe']) && isset($_GET['metodo'])){
+		$classe = $_GET['classe'].'Controller';
+		$metodo = $_GET['metodo'];
+		if (isset($_GET['id'])) {
+			$id = $_GET['id'];
+		}
+		
+	}
+	else if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+		$classe= $_POST['classe'].'Controller';
+		$metodo= $_POST['metodo'];
+		if (isset($_POST['id'])){
+			$id = $_POST['id'];
+		}
+	}
+	else{
+		$classe ="UsuarioController";
+		$metodo ="inicio";
+	}
+
+	$controller = new $classe();
+
+	if(!$id){
+		$controller->$metodo();
+	}	
+	else  {
+		$controller->$metodo($id);
+	}
+	
+
